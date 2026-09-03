06a604765d0055a3ccaae07be3a1a8400dae1ebc
Lamafps <hro1025@users.noreply.github.com>
2026-09-03 14:18:16 +0200

    started with chapter 3

--- manage.py
+++ manage.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
+
 import os
 import sys
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'apex.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "apex.settings")  # pyright: ignore[reportUnusedCallResult]
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
+            "Couldn't import Django. Are you sure it's installed and "  # pyright: ignore[reportImplicitStringConcatenation]
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
---
+++ polls/views.py
@@ -0,0 +1,22 @@
+from django.http import HttpResponse
+
+from polls.models import Question
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]  # pyright: ignore[reportAttributeAccessIssue]
+    output = ", ".join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)  # noqa: UP031
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)  # noqa: UP031
